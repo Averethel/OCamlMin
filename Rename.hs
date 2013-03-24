f3918a4845e5d515924c1a8645a5f4c16856a183
Krzysztof Sakwerda <ksakwerda@gmail.com>
2013-03-24 20:16:22 +0100

    + Rename now operates on typed AST

--- Rename.hs
+++ Rename.hs
@@ -1,44 +1,53 @@
 module Rename (rename) where
-  import Syntax.Expr
-  import Syntax.Pattern
+  import TypedSyntax.Expr
+  import TypedSyntax.Pattern
 
-  isBound :: String -> Pattern -> Bool
-  isBound n (Pvar x)      = x == n
-  isBound n (Ppair p1 p2) = isBound n p1 || isBound n p2
-  isBound n (Pcons p1 p2) = isBound n p1 || isBound n p2
-  isBound _ _             = False
+  isBound :: String -> TypedPattern -> Bool
+  isBound n (TPvar x _)      = x == n
+  isBound n (TPpair p1 p2 _) = isBound n p1 || isBound n p2
+  isBound n (TPcons p1 p2 _) = isBound n p1 || isBound n p2
+  isBound _ _                = False
 
-  renameFunClause :: String -> String -> FunClause -> FunClause
+  renameFunClause :: String -> String -> TypedFunClause -> TypedFunClause
   renameFunClause n1 n2 fc
-    | any (isBound n1) $ arguments fc = fc
-    | otherwise                       = fc { fbody = rename n1 n2 $ fbody fc }
+    | any (isBound n1) $ tfcArguments fc = fc
+    | otherwise                          = fc { tfcBody = rename n1 n2 $
+                                                          tfcBody fc }
 
-  renameCaseClause :: String -> String -> CaseClause -> CaseClause
+  renameCaseClause :: String -> String -> TypedCaseClause -> TypedCaseClause
   renameCaseClause n1 n2 cc
-    | n1 `elem` variables cc = cc
-    | otherwise              = cc { cbody = rename n1 n2 $ cbody cc }
+    | n1 `elem` map fst (tccVariables cc) = cc
+    | otherwise                           = cc { tccBody = rename n1 n2 $
+                                                            tccBody cc }
 
-  rename :: String -> String -> Expr -> Expr
-  rename n1 n2 (Evar n)
-    | n == n1                   = Evar n2
-    | otherwise                 = Evar n
-  rename n1 n2 (Efun fcs)       =
-    Efun $ map (renameFunClause n1 n2) fcs
-  rename n1 n2 (Elet p e1 e2)
-    | isBound n1 p              = Elet p e1 e2
-    | otherwise                 = Elet p (rename n1 n2 e1) $ rename n1 n2 e2
-  rename n1 n2 (Eletrec s fc e)
-    | s == n1                   = Eletrec s fc e
-    | otherwise                 = Eletrec s (map (renameFunClause n1 n2) fc) $
-                                          rename n1 n2 e
-  rename n1 n2 (Eapply e1 as)   = Eapply (rename n1 n2 e1) $
-                                         map (rename n1 n2) as
-  rename n1 n2 (Epair e1 e2)    = Epair (rename n1 n2 e1) $ rename n1 n2 e2
-  rename n1 n2 (Econs e1 e2)    = Econs (rename n1 n2 e1) $ rename n1 n2 e2
-  rename n1 n2 (Eif e1 e2 e3)   = Eif (rename n1 n2 e1) (rename n1 n2 e2) $
-                                      rename n1 n2 e3
-  rename n1 n2 (Eseq e1 e2)     = Eseq (rename n1 n2 e1) $ rename n1 n2 e2
-  rename n1 n2 (Ecase e ccs)    = Ecase (rename n1 n2 e) $
-                                        map (renameCaseClause n1 n2) ccs
-  rename n1 n2 (Ehandle e1 e2)  = Ehandle (rename n1 n2 e1) $ rename n1 n2 e2
-  rename _  _  e                = e
+  rename :: String -> String -> TypedExpr -> TypedExpr
+  rename n1 n2 (TEvar n t)
+    | n == n1                           = TEvar n2 t
+    | otherwise                         = TEvar n t
+  rename n1 n2 (TEfun fcs t)            =
+    TEfun (map (renameFunClause n1 n2) fcs) t
+  rename n1 n2 (TElet p e1 e2 t)
+    | isBound n1 p                      = TElet p e1 e2 t
+    | otherwise                         = TElet p (rename n1 n2 e1)
+                                                  (rename n1 n2 e2) t
+  rename n1 n2 (TEletrec s t1 fc e t2)
+    | s == n1                           = TEletrec s t1 fc e t2
+    | otherwise                         = TEletrec s t1
+                                            (map (renameFunClause n1 n2) fc)
+                                            (rename n1 n2 e) t2
+  rename n1 n2 (TEapply e1 as t)        = TEapply (rename n1 n2 e1)
+                                              (map (rename n1 n2) as) t
+  rename n1 n2 (TEpair e1 e2 t)         = TEpair (rename n1 n2 e1)
+                                                 (rename n1 n2 e2) t
+  rename n1 n2 (TEcons e1 e2 t)         = TEcons (rename n1 n2 e1)
+                                                 (rename n1 n2 e2) t
+  rename n1 n2 (TEif e1 e2 e3 t)        = TEif (rename n1 n2 e1)
+                                               (rename n1 n2 e2)
+                                               (rename n1 n2 e3) t
+  rename n1 n2 (TEseq e1 e2 t)          = TEseq (rename n1 n2 e1)
+                                                (rename n1 n2 e2) t
+  rename n1 n2 (TEcase e ccs t)         = TEcase (rename n1 n2 e)
+                                            (map (renameCaseClause n1 n2) ccs) t
+  rename n1 n2 (TEhandle e1 e2 t)       = TEhandle (rename n1 n2 e1)
+                                                   (rename n1 n2 e2) t
+  rename _  _  e                        = e
