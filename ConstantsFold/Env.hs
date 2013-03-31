112b946c48f1030d30ba2f18f4a532edba05768a
Krzysztof Sakwerda <ksakwerda@gmail.com>
2013-03-31 16:37:36 +0200

    + Constant folding on typed AST

--- ConstantsFold/Env.hs
+++ ConstantsFold/Env.hs
@@ -1,6 +1,8 @@
 module ConstantsFold.Env where
   import KNormal.KSyntax
 
+  import Types
+
   import Control.Exception.Base
 
   type Env = [(String, KExpr)]
@@ -13,33 +15,33 @@ module ConstantsFold.Env where
 
   memi :: String -> Env -> Bool
   memi x env = case x `lookup` env of
-    Just (KEint _) -> True
-    _              -> False
+    Just (KEint _ _) -> True
+    _                -> False
 
   findi :: String -> Env -> Integer
   findi x env = case x `lookup` env of
-    Just (KEint n) -> n
-    _              -> assert False $ findi x env
+    Just (KEint n _) -> n
+    _                -> assert False $ findi x env
 
   -- Just for the purpose of simplifying let expressions
   memp :: String -> Env -> Bool
   memp x env = case x `lookup` env of
-    Just (KEpair _ _) -> True
-    _                 -> False
+    Just (KEpair{}) -> True
+    _               -> False
 
-  findp :: String -> Env -> (String, String)
+  findp :: String -> Env -> ((String, Type), (String, Type))
   findp x env = case x `lookup` env of
-    Just (KEpair e1 e2) -> (e1, e2)
-    _                   -> assert False $ findp x env
+    Just (KEpair e1 e2 _) -> (e1, e2)
+    _                     -> assert False $ findp x env
 
   -- Just for the purpose of simplifying let expressions
   -- That's why we're only interested in x::xs here
   meml :: String -> Env -> Bool
   meml x env = case x `lookup` env of
-    Just (KEcons _ _) -> True
-    _                 -> False
+    Just (KEcons{}) -> True
+    _               -> False
 
-  findl :: String -> Env -> (String, String)
+  findl :: String -> Env -> ((String, Type), (String, Type))
   findl x env = case x `lookup` env of
-    Just (KEcons e1 e2) -> (e1, e2)
-    _                   -> assert False $ findl x env
+    Just (KEcons e1 e2 _) -> (e1, e2)
+    _                     -> assert False $ findl x env
