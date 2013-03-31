1ea0a4e3c49d82f10b6559b238be52476c852db3
Krzysztof Sakwerda <ksakwerda@gmail.com>
2013-03-31 16:46:00 +0200

    + Eliminating unused definitions on typed AST

--- EliminateDefinitions.hs
+++ EliminateDefinitions.hs
@@ -4,69 +4,69 @@ module EliminateDefinitions (eliminateDefinitions) where
   import Data.Set
 
   hasSideEffect  :: KExpr -> Bool
-  hasSideEffect (KEstore _ _)       = True
-  hasSideEffect (KEerror _)         = True
-  hasSideEffect (KEifEq _ _ e1 e2)  = hasSideEffect e1 || hasSideEffect e2
-  hasSideEffect (KEifLE _ _ e1 e2)  = hasSideEffect e1 || hasSideEffect e2
-  hasSideEffect (KElet _ e1 e2)     = hasSideEffect e1 || hasSideEffect e2
-  hasSideEffect (KEletRec _ e)      = hasSideEffect e
-  hasSideEffect (KEapply _ _)       = True
-  hasSideEffect (KEletPair _ _ _ e) = hasSideEffect e
-  hasSideEffect (KEletList _ _ _ e) = hasSideEffect e
-  hasSideEffect (KEhandle e1 e2)    = hasSideEffect e1 || hasSideEffect e2
-  hasSideEffect (KEseq e1 e2)       = hasSideEffect e1 || hasSideEffect e2
-  hasSideEffect (KEextFunApp _ _)   = True
-  hasSideEffect _                   = False
+  hasSideEffect (KEstore{})           = True
+  hasSideEffect (KEerror _ _)         = True
+  hasSideEffect (KEifEq _ _ e1 e2 _)  = hasSideEffect e1 || hasSideEffect e2
+  hasSideEffect (KEifLE _ _ e1 e2 _)  = hasSideEffect e1 || hasSideEffect e2
+  hasSideEffect (KElet _ e1 e2 _)     = hasSideEffect e1 || hasSideEffect e2
+  hasSideEffect (KEletRec _ e _)      = hasSideEffect e
+  hasSideEffect (KEapply{})           = True
+  hasSideEffect (KEletPair _ _ _ e _) = hasSideEffect e
+  hasSideEffect (KEletList _ _ _ e _) = hasSideEffect e
+  hasSideEffect (KEhandle e1 e2 _)    = hasSideEffect e1 || hasSideEffect e2
+  hasSideEffect (KEseq e1 e2 _)       = hasSideEffect e1 || hasSideEffect e2
+  hasSideEffect (KEextFunApp{})       = True
+  hasSideEffect _                     = False
 
   eliminateDefinitions :: KExpr -> IO KExpr
-  eliminateDefinitions (KEifEq s1 s2 e1 e2)   = do
+  eliminateDefinitions (KEifEq s1 s2 e1 e2 t)     = do
     e1' <- eliminateDefinitions e1
     e2' <- eliminateDefinitions e2
-    return $ KEifEq s1 s2 e1' e2'
-  eliminateDefinitions (KEifLE s1 s2 e1 e2)   = do
+    return $ KEifEq s1 s2 e1' e2' t
+  eliminateDefinitions (KEifLE s1 s2 e1 e2 t)     = do
     e1' <- eliminateDefinitions e1
     e2' <- eliminateDefinitions e2
-    return $ KEifLE s1 s2 e1' e2'
-  eliminateDefinitions (KElet s e1 e2)
+    return $ KEifLE s1 s2 e1' e2' t
+  eliminateDefinitions (KElet (s, t1) e1 e2 t2)
     | not (hasSideEffect e1) &&
-      not (s `member` freeVars e2)            = do
+      not (s `member` freeVars e2)                = do
         putStrLn $ "Eliminating variable: " ++ show s
         eliminateDefinitions e2
-    | otherwise                               = do
+    | otherwise                                   = do
       e1' <- eliminateDefinitions e1
       e2' <- eliminateDefinitions e2
-      return $ KElet s e1' e2'
-  eliminateDefinitions (KEletRec fd e)
-    | not (name fd `member` freeVars e)       = do
+      return $ KElet (s, t1) e1' e2' t2
+  eliminateDefinitions (KEletRec fd e t)
+    | not ((fst . name $ fd) `member` freeVars e) = do
       putStrLn $ "Eliminating function: " ++ show (name fd)
       eliminateDefinitions e
-    | otherwise                               = do
+    | otherwise                                   = do
       b' <- eliminateDefinitions $ body fd
       e' <- eliminateDefinitions e
-      return $ KEletRec fd { body = b' } e'
-  eliminateDefinitions (KEletPair s1 s2 s3 e)
-    | not (s1 `member` freeVars e) &&
-      not (s2 `member` freeVars e)            = do
+      return $ KEletRec fd { body = b' } e' t
+  eliminateDefinitions (KEletPair s1 s2 s3 e t)
+    | not (fst s1 `member` freeVars e) &&
+      not (fst s2 `member` freeVars e)            = do
         putStrLn $ "Eliminating variables: " ++ show s1 ++ ", " ++ show s2
         eliminateDefinitions e
-    | otherwise                               = do
+    | otherwise                                   = do
       e' <- eliminateDefinitions e
-      return $ KEletPair s1 s2 s3 e'
-  eliminateDefinitions (KEletList s1 s2 s3 e)
-    | not (s1 `member` freeVars e) &&
-      not (s2 `member` freeVars e)            = do
+      return $ KEletPair s1 s2 s3 e' t
+  eliminateDefinitions (KEletList s1 s2 s3 e t)
+    | not (fst s1 `member` freeVars e) &&
+      not (fst s2 `member` freeVars e)            = do
         putStrLn $ "Eliminating variables: " ++ show s1 ++ ", " ++ show s2
         eliminateDefinitions e
-    | otherwise                               = do
+    | otherwise                                   = do
       e' <- eliminateDefinitions e
-      return $ KEletList s1 s2 s3 e'
-  eliminateDefinitions (KEhandle e1 e2)       = do
+      return $ KEletList s1 s2 s3 e' t
+  eliminateDefinitions (KEhandle e1 e2 t)         = do
     e1' <- eliminateDefinitions e1
     e2' <- eliminateDefinitions e2
-    return $ KEhandle e1' e2'
-  eliminateDefinitions (KEseq e1 e2)          = do
+    return $ KEhandle e1' e2' t
+  eliminateDefinitions (KEseq e1 e2 t)            = do
     e1' <- eliminateDefinitions e1
     e2' <- eliminateDefinitions e2
-    return $ KEseq e1' e2'
-  eliminateDefinitions e                      =
+    return $ KEseq e1' e2' t
+  eliminateDefinitions e                          =
     return e
