f1acd0decb86b58fcc273336b9a670e8baf67cda
Krzysztof Sakwerda <ksakwerda@gmail.com>
2013-04-26 17:43:49 +0200

    + Registers

--- SPARC/Syntax.hs
+++ SPARC/Syntax.hs
@@ -66,3 +66,33 @@ module SPARC.Syntax where
     toplevel :: [FunDef],
     main     :: Seq
   } deriving (Eq, Show)
+
+  regs :: [String]
+  regs =
+    [ "%i2", "%i3", "%i4", "%i5",
+      "%l0", "%l1", "%l2", "%l3", "%l4", "%l5", "%l6", "%l7",
+      "%o0", "%o1", "%o2", "%o3", "%o4", "%o5" ]
+
+  -- closure address
+  regCl :: String
+  regCl = last regs
+
+  -- temporary for swap
+  regSw :: String
+  regSw = last . init $ regs
+
+  -- stack pointer
+  regSp :: String
+  regSp = "%i0"
+
+  -- heap pointer
+  regHp :: String
+  regHp = "%i1"
+
+  -- return address
+  regRa :: String
+  regRa = "%o7"
+
+  -- default staic failure label
+  failureLabel :: Label
+  failureLabel = L "_match_failure"
