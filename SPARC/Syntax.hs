24e072545e76621ddc73af1eb2ae6aa021038558
Krzysztof Sakwerda <ksakwerda@gmail.com>
2013-05-03 16:51:28 +0200

    + Floating point registers

--- SPARC/Syntax.hs
+++ SPARC/Syntax.hs
@@ -77,6 +77,12 @@ module SPARC.Syntax where
       "%l0", "%l1", "%l2", "%l3", "%l4", "%l5", "%l6", "%l7",
       "%o0", "%o1", "%o2", "%o3", "%o4", "%o5" ]
 
+  fregs :: [String]
+  fregs =
+    [ "%f0", "%f2", "%f4", "%f6", "%f8", "%f10",
+      "%f12", "%f14", "%f16", "%f18", "%f20",
+      "%f22", "%f24", "%f26", "%f28", "%f30" ]
+
   -- closure address
   regCl :: String
   regCl = last regs
