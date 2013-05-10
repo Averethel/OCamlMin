0ae4bdb500fec818f5fb299b27425e469f25dd02
Krzysztof Sakwerda <ksakwerda@gmail.com>
2013-05-10 13:18:45 +0200

    + Moved virtual SPARC syntax into separate module

--- SPARC/Syntax.hs
+++ SPARC/Syntax.hs
@@ -1,74 +1,8 @@
-module SPARC.Syntax where
-  import Types
-
-  data Label = L String deriving Eq
-
-  instance Show Label where
-    show (L s) = s
-
-  data IdOrIimm =
-      V String
-    | C Integer
-    deriving Eq
-
-  instance Show IdOrIimm where
-    show (V s) = s
-    show (C n) = show n
-
-  -- Instruction sequence
-  data Seq =
-      Ans Instr
-    | Let String Type Instr Seq
-    | Labeled Label Seq
-    deriving (Eq, Show)
-
-  -- SPARC assembly instructions
-  data Instr =
-      Inop
-    | Iset Integer
-    | IsetL Label
-    | Imov String
-    | Ineg String
-    | Iadd String IdOrIimm
-    | Isub String IdOrIimm
-    | ISLL String IdOrIimm
-    | Ild String  IdOrIimm
-    | Ist String String IdOrIimm
-    | IfMovD String
-    | IfNegD String
-    | IfAddD String String
-    | IfSubD String String
-    | IfMulD String String
-    | IfDivD String String
-    | IldDF String IdOrIimm
-    | IstDF String String IdOrIimm
-    | Icomment String
-    -- virtual instructions
-    | IifEq String IdOrIimm Seq Seq
-    | IifLE String IdOrIimm Seq Seq
-    | IifGE String IdOrIimm Seq Seq
-    | IifFEq String String Seq Seq
-    | IifFLE String String Seq Seq
-    -- closure address, integer arguments, and float arguments
-    | IcallCls String [String] [String]
-    | IcallDir Label [String] [String]
-    | Isave String String
-    | Irestore String
-    | Ijump Label
-    deriving (Eq, Show)
-
-  data FunDef = FD {
-    name  :: Label,
-    args  :: [String],
-    fargs :: [String],
-    body  :: Seq,
-    ret   :: Type
-  } deriving (Eq, Show)
-
-  data Program = P {
-    toplevel :: [FunDef],
-    main     :: Seq
-  } deriving (Eq, Show)
+module SPARC.Syntax (
+  regs, fregs, regCl, regSw, regSp, regHp, regRa, failureLabel,
+  Label(..), IdOrIimm(..), Seq(..), Instr(..), FunDef(..), Program(..)
+) where
+  import SPARC.Virtual.Syntax
 
   regs :: [String]
   regs =
---
+++ SPARC/Virtual/Syntax.hs
@@ -0,0 +1,71 @@
+module SPARC.Virtual.Syntax where
+  import Types
+
+  data Label = L String deriving Eq
+
+  instance Show Label where
+    show (L s) = s
+
+  data IdOrIimm =
+      V String
+    | C Integer
+    deriving Eq
+
+  instance Show IdOrIimm where
+    show (V s) = s
+    show (C n) = show n
+
+  -- Instruction sequence
+  data Seq =
+      Ans Instr
+    | Let String Type Instr Seq
+    | Labeled Label Seq
+    deriving (Eq, Show)
+
+  -- SPARC assembly instructions
+  data Instr =
+      Inop
+    | Iset Integer
+    | IsetL Label
+    | Imov String
+    | Ineg String
+    | Iadd String IdOrIimm
+    | Isub String IdOrIimm
+    | ISLL String IdOrIimm
+    | Ild String  IdOrIimm
+    | Ist String String IdOrIimm
+    | IfMovD String
+    | IfNegD String
+    | IfAddD String String
+    | IfSubD String String
+    | IfMulD String String
+    | IfDivD String String
+    | IldDF String IdOrIimm
+    | IstDF String String IdOrIimm
+    | Icomment String
+    -- virtual instructions
+    | IifEq String IdOrIimm Seq Seq
+    | IifLE String IdOrIimm Seq Seq
+    | IifGE String IdOrIimm Seq Seq
+    | IifFEq String String Seq Seq
+    | IifFLE String String Seq Seq
+    -- closure address, integer arguments, and float arguments
+    | IcallCls String [String] [String]
+    | IcallDir Label [String] [String]
+    | Isave String String
+    | Irestore String
+    | Ijump Label
+    deriving (Eq, Show)
+
+  data FunDef = FD {
+    name  :: Label,
+    args  :: [String],
+    fargs :: [String],
+    body  :: Seq,
+    ret   :: Type
+  } deriving (Eq, Show)
+
+  data Program = P {
+    toplevel :: [FunDef],
+    main     :: Seq
+  } deriving (Eq, Show)
